810c9e172acdcfee9711ccff22a6ffe0b7f79772
newt <hi@newty.dev>
2025-08-26 18:54:41 +0100

    feat: download tessdata

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
 import os
 from subprocess import run
 import random
+import requests
+
+# todo: don't include root files in sparse checkouts
 
 # configuration
 font = "Keposhka Regular"
@@ -8,16 +11,21 @@ lang = "eng"
 count = 100
 
 # download training data
-if not os.path.exists("langdata"):
-    run([
-        "git", "clone", "--filter=blob:none", "--no-checkout",
-        "https://github.com/tesseract-ocr/langdata.git"
-    ])
-    run(["git", "sparse-checkout", "init", "--cone"], cwd="langdata")
-run(["git", "sparse-checkout", "set", lang], cwd="langdata")
-run(["git", "checkout"], cwd="langdata")
+def clone(repo: str):
+    if not os.path.exists(repo):
+        run([
+            "git", "clone", "--filter=blob:none", "--no-checkout",
+            f"https://github.com/tesseract-ocr/{repo}.git"
+        ])
+        run(["git", "sparse-checkout", "init", "--cone"], cwd=repo)
+
+def select(repo: str, dir: str):
+    run(["git", "sparse-checkout", "set", dir], cwd=repo)
+    run(["git", "checkout"], cwd=repo)
 
 # read the training data
+clone("langdata")
+select("langdata", lang)
 training = f"langdata/{lang}/{lang}.training_text"
 with open(training, encoding="utf-8") as f:
     lines = [l.strip() for l in f.read().splitlines()]
@@ -50,3 +58,15 @@ for i, line in enumerate(lines):
         "--exposure=0",
         f"--unicharset_file=langdata/{lang}/{lang}.unicharset"
     ])
+
+# download tessdata
+clone("tesseract")
+select("tesseract", "tessdata")
+
+trained = f"{lang}.traineddata"
+trained_path = f"tesseract/tessdata/{trained}"
+if not os.path.exists(trained_path):
+    with requests.get(f"https://github.com/tesseract-ocr/tessdata_best/raw/refs/heads/main/{trained}") as res:
+        res.raise_for_status()
+        with open(trained_path, "wb") as f:
+            f.write(res.content)
